600de5641614be52dc770cfd91147e3cdb56d167
runqin <965993702@qq.com>
2021-09-08 09:49:59 +0800

    提交C#调用rust动态连接库技术验证demo
    验证想法：rust写个动态连接库，其他电脑不用安装rust环境，也能调用。提交C#调用rust动态连接库验证通过！

---
+++ C#调用rust动态链接库技术验证/CsharpDemo/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("rustdll.dll",EntryPoint = "output_int", CallingConvention = CallingConvention.Cdecl)]
+        //下面把 output_int 改成符合 C# 命名习惯的用法，不改就是 int output_int() 
+        private static extern int OutputInt(int n);
+        static int TestOutputInt(int n)
+        {
+            return OutputInt(n);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int o = TestOutputInt((int)numericUpDown1.Value);
+            textBox1.Text = o.ToString();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
