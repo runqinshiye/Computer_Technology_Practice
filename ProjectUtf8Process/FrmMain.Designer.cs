5aa34f69761ffccb83741ea6242832fd7da34b32
runqin <76540623+runqinshiye@users.noreply.github.com>
2022-01-18 17:39:59 +0800

    UTF8 Coding Project

---
+++ ProjectUtf8Process/FrmMain.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ProjectUtf8Process
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDirPath = new System.Windows.Forms.TextBox();
+            this.BtnConvert = new System.Windows.Forms.Button();
+            this.labMsg = new System.Windows.Forms.Label();
+            this.BtnChance = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "选择待转换工程根目录";
+            // 
+            // txtDirPath
+            // 
+            this.txtDirPath.Location = new System.Drawing.Point(154, 49);
+            this.txtDirPath.Name = "txtDirPath";
+            this.txtDirPath.Size = new System.Drawing.Size(504, 21);
+            this.txtDirPath.TabIndex = 1;
+            this.txtDirPath.Text = "D:\\MaiHe\\GitServer\\openDental20.3";
+            // 
+            // BtnConvert
+            // 
+            this.BtnConvert.Location = new System.Drawing.Point(227, 98);
+            this.BtnConvert.Name = "BtnConvert";
+            this.BtnConvert.Size = new System.Drawing.Size(287, 60);
+            this.BtnConvert.TabIndex = 2;
+            this.BtnConvert.Text = "转换";
+            this.BtnConvert.UseVisualStyleBackColor = true;
+            this.BtnConvert.Click += new System.EventHandler(this.BtnConvert_Click);
+            // 
+            // labMsg
+            // 
+            this.labMsg.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.labMsg.Location = new System.Drawing.Point(25, 167);
+            this.labMsg.Name = "labMsg";
+            this.labMsg.Size = new System.Drawing.Size(714, 45);
+            this.labMsg.TabIndex = 3;
+            this.labMsg.Text = "提示信息……";
+            // 
+            // BtnChance
+            // 
+            this.BtnChance.Location = new System.Drawing.Point(664, 47);
+            this.BtnChance.Name = "BtnChance";
+            this.BtnChance.Size = new System.Drawing.Size(75, 23);
+            this.BtnChance.TabIndex = 4;
+            this.BtnChance.Text = "选择";
+            this.BtnChance.UseVisualStyleBackColor = true;
+            this.BtnChance.Click += new System.EventHandler(this.BtnChance_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 221);
+            this.Controls.Add(this.BtnChance);
+            this.Controls.Add(this.labMsg);
+            this.Controls.Add(this.BtnConvert);
+            this.Controls.Add(this.txtDirPath);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "转换工程源码为UTF8编码";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDirPath;
+        private System.Windows.Forms.Button BtnConvert;
+        private System.Windows.Forms.Label labMsg;
+        private System.Windows.Forms.Button BtnChance;
+    }
+}
+
---
+++ ProjectUtf8Process/FrmMain.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using UtfUnknown;
+
+namespace ProjectUtf8Process
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+        int icount = 0;
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            String folderPath = @txtDirPath.Text.Trim();
+
+            ParseDirectory(@folderPath, (filePath) =>
+            {
+                // Detect from File (NET standard 1.3+ or .NET 4+)
+                DetectionResult result = CharsetDetector.DetectFromFile(filePath); // or pass FileInfo
+                
+                // Get the best Detection
+                DetectionDetail resultDetected = result.Detected;
+                if (resultDetected != null) 
+                {
+                // Get the alias of the found encoding
+                string encodingName = resultDetected.EncodingName;
+
+                // Get the System.Text.Encoding of the found encoding (can be null if not available)
+                Encoding encoding = resultDetected.Encoding;
+
+                // Get the confidence of the found encoding (between 0 and 1)
+                float confidence = resultDetected.Confidence;
+
+                // Get all the details of the result
+                IList<DetectionDetail> allDetails = result.Details;
+
+                    ////&& !encoding.ToString().Equals("System.Text.SBCSCodePageEncoding")
+                    if (encoding != null && encoding != Encoding.UTF8)
+                    {
+                        string text = "";
+                        using (StreamReader read = new StreamReader(filePath, encoding))
+                        {
+                            string oldtext = read.ReadToEnd();
+                            text = oldtext;
+                            text = text.Replace("\n", "\r\n");
+                            text = text.Replace("\r\r\n", "\r\n"); // 防止替换了正常的换行符      
+                            if (oldtext.Length == text.Length)
+                            {
+                                labMsg.Text = filePath.Substring(filePath.LastIndexOf("\\") + 1) + " 不需要标准化";
+                            }
+                        }
+                        File.WriteAllText(filePath, text, Encoding.UTF8); //utf-8格式保存，防止乱码
+
+                        icount++;
+
+                        Console.WriteLine(icount.ToString() + ">>>" + filePath.Substring(filePath.LastIndexOf("\\") + 1) + " 行尾标准化完成");
+
+                        labMsg.Text = filePath.Substring(filePath.LastIndexOf("\\") + 1) + " 行尾标准化完成";
+
+                        Application.DoEvents();
+                    }
+                }
+            });
+        }
+
+
+
+        /// <summary>递归所有的目录，根据过滤器找到文件，并使用委托来统一处理</summary>
+        /// <param name="info"></param>
+        /// <param name="filter"></param>
+        /// <param name="action"></param>
+        private void ParseDirectory(string folderPath, Action<string> action)
+        {
+            if (string.IsNullOrWhiteSpace(folderPath))
+                return;
+
+            labMsg.Text = "读取目录：" + folderPath;
+            Application.DoEvents();
+            // 处理文件
+            // 处理文件
+            var fileNameArray = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories).Where(s => s.EndsWith(".cs") || s.EndsWith(".vb")); 
+            if (fileNameArray.Count() > 0)
+            {
+                foreach (var filePath in fileNameArray)
+                {
+                    action(filePath);
+                }
+            }
+            else
+            {
+                Console.WriteLine("未发现文件！");
+            }
+            //得到子目录，递归处理
+            string[] dirs = Directory.GetDirectories(folderPath);
+            var iter = dirs.GetEnumerator();
+            while (iter.MoveNext())
+            {
+                string str = (string)(iter.Current);
+                ParseDirectory(str, action);
+            }
+        }
+
+
+        private void BtnChance_Click(object sender, EventArgs e)
+        {
+
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "请选择待转换工程根目录";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    MessageBox.Show(this, "路径不能为空", "提示");
+                    return;
+                }
+                txtDirPath.Text = dialog.SelectedPath;
+                BtnConvert.Enabled = true;
+            }
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            labMsg.Text = "";
+        }
+    }
+}
