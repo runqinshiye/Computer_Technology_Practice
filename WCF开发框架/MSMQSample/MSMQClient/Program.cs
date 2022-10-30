f468252e99eb977498bd3b89f7cef9ab6d761f6b
hagedu <eohagedu@gmail.com>
2022-10-30 18:25:16 +0800

    提交msmq消息队列模式

---
+++ WCF开发框架/MSMQSample/MSMQClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Messaging; // 需要添加System.Messaging引用
+
+namespace MSMQClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (MessageQueue.Exists(@".\Private$\LearningHardMSMQ"))
+            {
+                // 创建消息队列对象
+                using (MessageQueue mq = new MessageQueue(@".\Private$\LearningHardMSMQ"))
+                {
+                    // 设置消息队列的格式化器
+                    mq.Formatter = new XmlMessageFormatter(new string[] { "System.String" });
+                    foreach (Message msg in mq.GetAllMessages())
+                    {
+                        Console.WriteLine("Received Private Message is: {0}", msg.Body);
+                    }
+
+                    Message firstmsg = mq.Receive(); // 获得消息队列中第一条消息
+                    Console.WriteLine("Received The first Private Message is: {0}", firstmsg.Body);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ WCF开发框架/MSMQSample/MSMQServer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Messaging;
+
+namespace MSMQServer
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            // 创建一个公共队列,公共队列只能创建在域环境里
+            //if (!MessageQueue.Exists(@".\LearningHardMSMQ")) // 判断此路径下是否已经有该队列
+            //{
+            //    using (MessageQueue mq = MessageQueue.Create(@".\LearningHardMSMQ"))
+            //    {
+            //        mq.Label = "LearningHardQueue"; // 设置队列标签
+            //        Console.WriteLine("已经创建了一个公共队列");
+            //        Console.WriteLine("路径为:{0}", mq.Path);
+            //        Console.WriteLine("队列名字为:{0}", mq.QueueName);
+            //        mq.Send("MSMQ Message", "Leaning Hard"); // 发送消息
+            //    }
+            //}
+
+            //if (MessageQueue.Exists(@".\Private$\LearningHardMSMQ"))
+            //{
+            //    // 删除消息队列
+            //    MessageQueue.Delete(@".\Private$\LearningHardMSMQ");
+            //}
+            // 创建一个私有消息队列
+            if (!MessageQueue.Exists(@".\Private$\LearningHardMSMQ"))
+            {
+                using (MessageQueue mq = MessageQueue.Create(@".\Private$\LearningHardMSMQ"))
+                {
+                    mq.Label = "LearningHardPrivateQueue"; 
+                    Console.WriteLine("已经创建了一个私有队列");
+                    Console.WriteLine("路径为:{0}", mq.Path);
+                    Console.WriteLine("私有队列名字为:{0}", mq.QueueName);
+                    mq.Send("MSMQ Private Message", "Leaning Hard"); // 发送消息
+                }
+            }
+
+            // 遍历所有的公共消息队列
+            //foreach (MessageQueue mq in MessageQueue.GetPublicQueues())
+            //{
+            //    mq.Send("Sending MSMQ public message" + DateTime.Now.ToLongDateString(), "Learning Hard");
+            //    Console.WriteLine("Public Message is sent to {0}", mq.Path);
+            //}
+           
+            if (MessageQueue.Exists(@".\Private$\LearningHardMSMQ")) 
+            {
+                // 获得私有消息队列
+                MessageQueue mq = new MessageQueue(@".\Private$\LearningHardMSMQ");
+                mq.Send("Sending MSMQ private message" + DateTime.Now.ToLongDateString(), "Leaning Hard");
+                Console.WriteLine("Private Message is sent to {0}", mq.Path);
+            }
+
+            Console.Read();
+        }
+    }
+}
